3a23f54bb15344830d35b2eae34204f6a64380fa
Semen Bevzuk <semen.bevzuk@gmail.com>
2015-11-14 14:14:05 +0300

    Шаблонный Stack и тесты

--- include/stack.h
+++ include/stack.h
@@ -3,15 +3,16 @@ using namespace std;
 
 #define MemSize 30						 //стандартный размер
 
+template <class Type>
 class Stack
 {
 private:
-	int *pMem;
+	Type *pMem;
 	int Size;
 	int Top;
 
 	int GetSize() const { return Size; }
-	int* GetpMem() const { return pMem; }
+	//int* GetpMem() const { return pMem; }
 public:
 	Stack();
 	Stack(int n);
@@ -21,9 +22,9 @@ public:
 	bool IsFull();
 	bool IsEmpty();
 
-	int Put();								//взять и удалить верхний элемент
+	Type Put();								//взять и удалить верхний элемент
 	void Push(int element);					//добавить элемент
-	int Peek() const;						//посмотреть верхний элемент без удаления
+	Type Peek() const;						//посмотреть верхний элемент без удаления
 
 	void SetSize(int n);
 	
@@ -31,3 +32,145 @@ public:
 	bool operator==(const Stack &s) const;  
 	bool operator!=(const Stack &s) const;  
 };
+
+template <class Type>
+Stack<Type>::Stack() {
+	pMem = new Type[MemSize];
+	Size = MemSize;
+	Top = -1;
+	for (int i = 0; i < Size; i++) {
+		pMem[i] = 0;
+	}
+}
+
+template <class Type>
+Stack<Type>::Stack(int n) {
+	if (n<0) {
+		throw invalid_argument("Размер стэка < 0, конструктор");
+	}
+	pMem = new Type[n];
+	Size = n;
+	Top = -1;
+	for (int i = 0; i < Size; i++) {
+		pMem[i] = 0;
+	}
+}
+
+template <class Type>
+Stack<Type>::~Stack() {
+	delete[]pMem;
+}
+
+template <class Type>
+Stack<Type>::Stack(const Stack &s) {
+	Size = s.GetSize();
+	pMem = new Type[Size];
+	Top = s.Top;
+
+	for (int i = 0; i < Size; i++) {
+		pMem[i] = s.pMem[i];
+	}
+}
+
+template<class Type>
+bool Stack<Type>::IsEmpty() {
+	if (Top == -1) {
+		return true;
+	}
+	return false;
+}
+
+template<class Type>
+bool Stack<Type>::IsFull() {
+	if (Top == Size - 1) {
+		return true;
+	}
+	return false;
+}
+
+template <class Type>
+void Stack<Type>::Push(int element) {
+	if (this->IsFull()) {
+		throw ("Стэк переполнен!");
+	}
+	//Top++;
+	pMem[++Top] = element;
+}
+
+template <class Type>
+Type Stack<Type>::Put() {
+	if (this->IsEmpty()) {
+		throw ("Стэк пуст!");
+	}
+	return pMem[Top--];
+}
+
+template <class Type>
+Type Stack<Type>::Peek() const {
+	if (Top == -1) {
+		throw ("Стэк пуст!");
+	}
+	return pMem[Top];
+}
+
+template <class Type>
+void Stack<Type>::SetSize(int n) {
+	if (n<0) {
+		throw invalid_argument("Размер стэка < 0");
+	}
+	if (n<Top + 1) {
+		throw invalid_argument("New Size < Top");
+	}
+
+	Type *p = new Type[Top + 1];
+	for (int i = 0; i < Top + 1; i++) {
+		p[i] = pMem[i];
+	}
+
+	Size = n;
+	delete[]pMem;
+	pMem = new Type[Size];
+	for (int i = 0; i < Top + 1; i++) {
+		pMem[i] = p[i];
+	}
+	for (int i = Top + 1; i < Size; i++) {
+		pMem[i] = 0;
+	}
+}
+
+template <class Type>
+Stack<Type>& Stack<Type>::operator=(const Stack& s) {
+	if (pMem == s.pMem) {
+		return *this;
+	}
+	Size = s.Size;
+	Top = s.Top;
+	delete[]pMem;
+	pMem = new Type[Size];
+	for (int i = 0; i < Size; i++) {
+		pMem[i] = s.pMem[i];
+	}
+	return *this;
+}
+
+template <class Type>
+bool Stack<Type>::operator==(const Stack& s) const {
+	if ((Size == s.Size) && (Top == s.Top)) {
+		for (int i = 0; i < Size; i++) {
+			if (pMem[i] != s.pMem[i]) {
+				return false;
+			}
+		}
+		return true;
+	}
+	return false;
+}
+
+template <class Type>
+bool Stack<Type>::operator!=(const Stack &s) const {
+	if ((*this) == s) {
+		return false;
+	}
+	return true;
+}
+
--- test/test_stack.cpp
+++ test/test_stack.cpp
@@ -2,23 +2,23 @@
 #include <gtest.h>
 
 TEST(Stack, can_create_stack) {
-	ASSERT_NO_THROW(Stack a);
+	ASSERT_NO_THROW(Stack<double> a);
 }
 TEST(Stack, can_create_stack_whith_size) {
-	ASSERT_NO_THROW(Stack a(20));
+	ASSERT_NO_THROW(Stack<double> a(20));
 }
 TEST(Stack, can_not_create_stack_whith_negativ_length) {
-	ASSERT_ANY_THROW(Stack a(-10));
+	ASSERT_ANY_THROW(Stack<double> a(-10));
 }
 TEST(Stack, can_add_element) {
-	Stack a(10);
+	Stack<double> a(10);
 
 	int b=1;
 
 	ASSERT_NO_THROW(a.Push(b));
 }
 TEST(Stack, can_not_add_element_in_full_stack) {
-	Stack a(10);
+	Stack<double> a(10);
 	int b = 1;
 	for (int i = 0; i < 10; i++)
 	{
@@ -27,49 +27,49 @@ TEST(Stack, can_not_add_element_in_full_stack) {
 	ASSERT_ANY_THROW(a.Push(b));
 }
 TEST(Stack, can_pick_element) {
-	Stack a(10);
+	Stack<double> a(10);
 	int b = 1;
 	a.Push(b);
 	EXPECT_EQ(b, a.Put());
 }
 TEST(Stack, can_pick_and_not_delete_element) {
 
-	Stack a(10);
+	Stack<double> a(10);
 	int b = 1;
-	int c;
+	double c;
 	a.Push(b);
 	c = a.Peek();
 	EXPECT_EQ(b, a.Put());
 }
 TEST(Stack, can_not_put_from_empty_stack) {
-	Stack a(10);
-	int b;
+	Stack<double> a(10);
+	double b;
 	ASSERT_ANY_THROW(b = a.Put());
 }
 TEST(Stack, can_copy_Stack) {
-	Stack a(10);
-	ASSERT_NO_THROW(Stack b(a));
+	Stack<double> a(10);
+	ASSERT_NO_THROW(Stack<double> b(a));
 }
 TEST(Stack, copied_stack_are_equal) {
-	Stack a(10);
-	Stack b(a);
+	Stack<double> a(10);
+	Stack<double> b(a);
 	EXPECT_EQ(b, a);
 }
 TEST(Stack, copied_pMem_are_not_equal) {
-	Stack a(10);
-	Stack b(a);
+	Stack<double> a(10);
+	Stack<double> b(a);
 	a.Push(1);
 	b.Push(2);
 	EXPECT_NE(b.Put(), a.Put());
 }
 
 TEST(Stack, can_not_SetSize_negativ_length) {
-	Stack a(10);
+	Stack<double> a(10);
 	a.SetSize(15);
 	ASSERT_ANY_THROW(a.SetSize(-10));
 }
 TEST(Stack, can_SetSize_then_Size_larger_n) {
-	Stack a(10);
+	Stack<double> a(10);
 	for (int i = 0; i < 10; i++)
 	{
 		a.Push(i);
@@ -77,7 +77,7 @@ TEST(Stack, can_SetSize_then_Size_larger_n) {
 	ASSERT_ANY_THROW(a.SetSize(5));
 }
 TEST(Stack, can_SetSize) {
-	Stack a(10);
+	Stack<double> a(10);
 	for (int i = 0; i < 10; i++) {
 		a.Push(i);
 	}
@@ -85,18 +85,18 @@ TEST(Stack, can_SetSize) {
 	ASSERT_NO_THROW(a.Push(1));
 }
 TEST(Stack, can_assign_stacks) {
-	Stack a(2);
+	Stack<double> a(2);
 	for (int i = 0; i < 2; i++) {
 		a.Push(i);
 	}
-	Stack b(2);
+	Stack<double> b(2);
 	b = a;
 	EXPECT_EQ(1, b.Put());
 	EXPECT_EQ(0, b.Put());
 }
 TEST(Stack, assign_stacks_are_equal) {
-	Stack a(10);
-	Stack b(10);
+	Stack<double> a(10);
+	Stack<double> b(10);
 
 	for (int i = 0; i < 10; i++) {
 		a.Push(i);
@@ -106,28 +106,28 @@ TEST(Stack, assign_stacks_are_equal) {
 	EXPECT_EQ(a,b);
 }
 TEST(Stack, stack_equal_itself) {
-	Stack a(10);
+	Stack<double> a(10);
 	for (int i = 0; i < 10; i++) {
 		a.Push(i);
 	}
 	EXPECT_EQ(a, a);
 }
 TEST(Stack, stacks_whith_different_size_not_equal) {
-	Stack a(10);
-	Stack b(5);
+	Stack<double> a(10);
+	Stack<double> b(5);
 	EXPECT_NE(a, b);
 }
 TEST(Stack, different_stacks_not_equal) {
-	Stack a(10);
-	Stack b(10);
+	Stack<double> a(10);
+	Stack<double> b(10);
 
 	a.Push(1);
 
 	EXPECT_NE(a, b);
 }
 TEST(Stack, stacks_whith_different_Top_not_equal) {
-	Stack a(10);
-	Stack b(10);
+	Stack<double> a(10);
+	Stack<double> b(10);
 	b.Push(0);
 	EXPECT_NE(a, b);
 }
